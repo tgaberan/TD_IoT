f76e3cc29ce86cc4f8c8ebc6cc4d942ace1f8c87
t.gaberan <t.gaberan@technologyandstrategy.com>
2024-03-13 00:09:27 +0100

    add first version of IoT Client

---
+++ IoT_TD.py
@@ -0,0 +1,68 @@
+# python 3.6
+
+import random
+import json
+import time
+
+from paho.mqtt import client as mqtt_client
+
+
+broker = '213.32.88.153'
+port = 1883
+topic = "/1/ctrl"
+# Generate a Client ID with the publish prefix.
+client_id = f'publish-{random.randint(0, 1000)}'
+username = 'englab'
+password = 'englab'
+
+
+import paho.mqtt.client as mqtt
+
+def on_subscribe(client, userdata, mid, reason_code_list, properties):
+    # Since we subscribed only for a single channel, reason_code_list contains
+    # a single entry
+    if reason_code_list[0].is_failure:
+        print(f"Broker rejected you subscription: {reason_code_list[0]}")
+    else:
+        print(f"Broker granted the following QoS: {reason_code_list[0].value}")
+
+def on_unsubscribe(client, userdata, mid, reason_code_list, properties):
+    # Be careful, the reason_code_list is only present in MQTTv5.
+    # In MQTTv3 it will always be empty
+    if len(reason_code_list) == 0 or not reason_code_list[0].is_failure:
+        print("unsubscribe succeeded (if SUBACK is received in MQTTv3 it success)")
+    else:
+        print(f"Broker replied with failure: {reason_code_list[0]}")
+    client.disconnect()
+
+def on_message(client, userdata, message):
+    # userdata is the structure we choose to provide, here it's a list()
+    wMsgPayload = message.payload
+    print(wMsgPayload)
+    m_in = json.loads(wMsgPayload)
+    if( m_in["led"] == 1 ):
+        print("led ON")
+    else:
+        print("led OFF")
+    # We only want to process 10 messages
+    #client.unsubscribe("#")
+
+def on_connect(client, userdata, flags, reason_code, properties):
+    if reason_code.is_failure:
+        print(f"Failed to connect: {reason_code}. loop_forever() will retry connection")
+    else:
+        # we should always subscribe from on_connect callback to be sure
+        # our subscribed is persisted across reconnections.
+        client.subscribe("#")
+
+mqttc = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
+mqttc.on_connect = on_connect
+mqttc.on_message = on_message
+mqttc.on_subscribe = on_subscribe
+mqttc.on_unsubscribe = on_unsubscribe
+
+mqttc.username_pw_set("englab","englab")
+mqttc.connect(broker)
+mqttc.loop_forever()
+#print(f"Received the following message: {mqttc.user_data_get()}")
+print("exit")
--- README.md
+++ README.md
@@ -3,13 +3,68 @@
 
 ##Start linux
 
+
+open putty : https://the.earth.li/~sgtatham/putty/latest/w64/putty.exe or teraterm or any SSH terminal
+
+connect to 192.168.7.2
+login : debian
+pass : temppwd
+
+
 ###Connect to the WiFi
+
+root@BeagleBone:~# wpa_cli -i wlan0
+wpa_cli v2.9
+Copyright (c) 2004-2019, Jouni Malinen <j@w1.fi> and contributors
+
+This software may be distributed under the terms of the BSD license.
+See README for more details.
+
+Interactive mode
+
+> scan
+OK
+<3>CTRL-EVENT-SCAN-STARTED 
+<3>CTRL-EVENT-SCAN-RESULTS 
+<3>CTRL-EVENT-NETWORK-NOT-FOUND 
+> scan_results
+bssid / frequency / signal level / flags / ssid
+b4:fb:e4:f5:7b:d2	5180	-86	[WPA2-PSK-CCMP][ESS]	XYZA
+be:fb:e4:f5:7b:d2	5180	-87	[WPA2-PSK-CCMP][ESS]	BeagleBone
+ba:fb:e4:f5:7b:d2	5180	-87	[WPA2-PSK-CCMP][ESS]	XYZB
+c2:fb:e4:f5:7b:d2	5180	-87	[WPA2-PSK-CCMP][ESS]	XYZC
+> 
+> add_network
+1
+> set_network 1 ssid "BeagleBone"
+OK
+> set_network 1 psk "BeagleBone"
+OK
+> enable_network 1
+OK
+<3>CTRL-EVENT-SCAN-STARTED 
+<3>CTRL-EVENT-SCAN-RESULTS 
+<3>SME: Trying to authenticate with be:fb:e4:f5:7b:d2 (SSID='BeagleBone' freq=5180 MHz)
+<3>Trying to associate with be:fb:e4:f5:7b:d2 (SSID='BeagleBone' freq=5180 MHz)
+<3>Associated with be:fb:e4:f5:7b:d2
+<3>CTRL-EVENT-SUBNET-STATUS-UPDATE status=0
+<3>WPA: Key negotiation completed with be:fb:e4:f5:7b:d2 [PTK=CCMP GTK=CCMP]
+<3>CTRL-EVENT-CONNECTED - Connection to be:fb:e4:f5:7b:d2 completed [id=1 id_str=]
+
 
 ###Test Internet connection 
 
 ping google.com
 
 expected results : 
+PING google.com(par10s49-in-x0e.1e100.net (2a00:1450:4007:80c::200e)) 56 data bytes
+64 bytes from par10s49-in-x0e.1e100.net (2a00:1450:4007:80c::200e): icmp_seq=1 ttl=115 time=50.8 ms
+64 bytes from par21s11-in-x0e.1e100.net (2a00:1450:4007:80c::200e): icmp_seq=2 ttl=115 time=16.6 ms
+64 bytes from par21s11-in-x0e.1e100.net (2a00:1450:4007:80c::200e): icmp_seq=3 ttl=115 time=17.3 ms
+64 bytes from par21s11-in-x0e.1e100.net (2a00:1450:4007:80c::200e): icmp_seq=4 ttl=115 time=15.2 ms
+
+stop : ctrl+c
+
 
 
 
